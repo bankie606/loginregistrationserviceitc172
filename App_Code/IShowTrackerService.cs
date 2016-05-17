88de27d6dfb9ed7ed8a72cc1dc163a52559301ed
bankie606 <frcrescioni@gmail.com>
2016-05-17 10:31:51 -0700

    Add files via upload

---
+++ App_Code/IShowTrackerService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IShowTrackerService" in both code and config file together.
+[ServiceContract]
+public interface IShowTrackerService
+{
+
+    [OperationContract]
+    bool RegisterVenue(Venue v, VenueLogin vl);
+
+    [OperationContract]
+
+    int VenueLogin(string userName, string password);
+}
+
+    [DataContract]
+    public class VenueLite
+    {
+        //if you don't add DataMember it won't throw an error but it won't allow you to see the field
+        [DataMember]
+        public string VenueName { set; get; }
+
+        [DataMember]
+        public string VenueAddress { set; get; }
+
+        [DataMember]
+        public string VenueCity { set; get; }
+
+        [DataMember]
+        public DateTime VenueDateAdded { set; get; }
+    }
+
+
+
+
---
+++ App_Code/ShowTrackerService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ShowTrackerService" in code, svc and config file together.
+public class ShowTrackerService : IShowTrackerService
+{
+    ShowTrackerEntities db = new ShowTrackerEntities();
+    public bool RegisterVenue(Venue v, VenueLogin vl)
+    {
+        bool result = true;
+        int pass = db.usp_RegisterVenue(v.VenueName, v.VenueAddress, v.VenueCity, v.VenueState, v.VenueZipCode, v.VenuePhone, v.VenueEmail, v.VenueWebPage, v.VenueAgeRestriction, vl.VenueLoginUserName, vl.VenueLoginPasswordPlain);
+        if (pass == -1)
+        {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public int VenueLogin(string userName, string password)
+    {
+        int venueLoginKey = 0;
+        int result = db.usp_venueLogin(userName, password);
+        if (result != -1)
+        {
+            var key = (from r in db.VenueLogins
+                       where r.VenueLoginUserName.Equals(userName)
+                       select new { r.VenueLoginKey }).FirstOrDefault();
+
+            venueLoginKey = key.VenueLoginKey;
+
+        }
+
+        return venueLoginKey;
+    }
+}
